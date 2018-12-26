2cc846d8f09aac24f99162e9f96f90ac43980177
robingw <robingw@163.com>
2018-12-26 13:46:41 +0800

    增加WebShell工程。

---
+++ WebShell/WebShell/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+namespace WebShell
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            webView.On<Android>().SetMixedContentMode(MixedContentHandling.AlwaysAllow);
+        }
+    }
+}
